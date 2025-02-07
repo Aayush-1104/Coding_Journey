5acebc0b681183ce1756cb00b051fb4e573d5b1d
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-02-07 15:06:39 +0530

    Time: 5 ms (72.43%), Space: 44 MB (60.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int num=0;
+        while(x>0){
+            int temp =x%10;
+            x/=10;
+            num=num*10+temp;
+        }
+        return num==original;
+    }
+}
