bdc6b281d7c960c1ea2c469803592d08937d5e95
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-03-02 01:25:47 +0530

    Time: 0 ms (100%), Space: 41.2 MB (25.82%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
+        // Brian kerningham's method
+        if(n==-2147483648){
             return false;
         }
-        while(n%2==0){
-            n/=2;
-        }
-        return n==1;
+        return n!=0 && ((n & (n-1))==0);
+
+
+        // if(n==0){
+        //     return false;
+        // }
+        // while(n%2==0){
+        //     n/=2;
+        // }
+        // return n==1;
     }
 }
