eca728e954a68ee13284b43f3c55c4d86e60d721
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-03-05 20:55:01 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,38 @@
 class Solution {
+    public double powerX(double x,long n){
+        if(x==0) return 0;
+        if(n==0) return 1;
+        if(n%2==0) return powerX(x*x,n/2); //if n is even
+        else return x*powerX(x*x,(n-1)/2); //if n is odd
+    }
     public double myPow(double x, int n) {
-        if(n==0){
-            return 1.0;
-        }
-        long exp=n;
+        // // Mathematics method3
+        // if(n==0){
+        //     return 1.0;
+        // }
+        // long exp=n;
+        // if(n<0){
+        //     x=1/x;
+        //     exp=-exp;
+        // }
+        // double temp=myPow(x,(int)(exp/2));
+        // temp*=temp;
+        // if(exp%2==0){
+        //     return temp;
+        // }
+        // else{
+        //     return temp*x;
+        // }
+
+        // Using Recursion
+        double pow=0;
+        long absN=n;
         if(n<0){
-            x=1/x;
-            exp=-exp;
-        }
-        double temp=myPow(x,(int)(exp/2));
-        temp*=temp;
-        if(exp%2==0){
-            return temp;
+            pow=1.0/powerX(x,-absN);
         }
         else{
-            return temp*x;
+            pow=powerX(x,absN);
         }
+        return pow;
     }   
 }
