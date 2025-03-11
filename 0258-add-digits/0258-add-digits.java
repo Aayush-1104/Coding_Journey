9b0e334c6372b05df35df9e266934868d34778d7
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-03-11 20:27:55 +0530

    Time: 0 ms (100%), Space: 41.4 MB (13.62%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,8 +1,13 @@
 class Solution {
     public int addDigits(int num){
-       if(num<=9){
-        return num;
-       }
-       return addDigits(num%10+num/10);
+        // another logic
+        if(num==0) return 0;
+        else if(num%9==0) return 9;
+        else return num%9;
+    //    // My recursive code
+    //    if(num<=9){
+    //     return num;
+    //    }
+    //    return addDigits(num%10+num/10);
     }
 }
