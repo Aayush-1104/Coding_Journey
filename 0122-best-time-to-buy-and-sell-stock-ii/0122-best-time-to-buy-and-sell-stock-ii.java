d5f593be0db125f217ead6cce6b135fe93fef517
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-06-03 12:51:52 +0530

    Time: 1 ms (79.06%), Space: 45.9 MB (47.16%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int n=prices.length;
         for(int i=1;i<n;i++){
             if(prices[i]>prices[i-1]){
-                profit+=prices[i]-prices[i-1];
+                profit+=(prices[i]-prices[i-1]);
             }
         }
         return profit;
