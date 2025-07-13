051797095e8714ef3cf0a4548a3eacd7c655995c
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-07-14 00:02:56 +0530

    Time: 13 ms (42.37%), Space: 45.1 MB (29.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,24 @@
 class Solution {
-    public boolean palindrome(String s, int start, int end){
-        if(start>=end){
-            return true;
-        }
-        return ((s.charAt(start)==s.charAt(end)) && palindrome(s,start+1,end-1));
-    }
+    // public boolean palindrome(String s, int start, int end){
+    //     if(start>=end){
+    //         return true;
+    //     }
+    //     return ((s.charAt(start)==s.charAt(end)) && palindrome(s,start+1,end-1));
+    // }
     public boolean isPalindrome(String s) {
         String t=s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        int start=0;
-        int end=t.length(); 
-        return palindrome(t,start,end-1);
+        // int start=0;
+        // int end=t.length(); 
+        // return palindrome(t,start,end-1);
+        int i=0;
+        int j=t.length()-1;
+        while(i<j){
+            if(t.charAt(i)!=t.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
