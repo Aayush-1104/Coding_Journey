9433a8a2fd41610e948b4d3a343151124012d8ee
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-07-20 12:52:06 +0530

    Time: 0 ms (100%), Space: 42.2 MB (65.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,18 +1,40 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i=0;
-        int j=matrix[0].length-1;
-        while(i<matrix.length && j>=0){
-            if(matrix[i][j]==target){
+        // Binary Search Approach 
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int low=0;
+        int high=n*m-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target){
                 return true;
             }
-            else if(matrix[i][j]>target){
-                j--;
+            else if(matrix[row][col]<target){
+                low=mid+1;
             }
             else{
-                i++;
+                high=mid-1;
             }
         }
         return false;
+
+        // // Traversal: Top right Approach 
+        // int i=0;
+        // int j=matrix[0].length-1;
+        // while(i<matrix.length && j>=0){
+        //     if(matrix[i][j]==target){
+        //         return true;
+        //     }
+        //     else if(matrix[i][j]>target){
+        //         j--;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // return false;
     }
 }
