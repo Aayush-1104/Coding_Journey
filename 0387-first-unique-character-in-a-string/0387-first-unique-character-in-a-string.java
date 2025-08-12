409ebaf9278c70ce5cc543bff59bdeec79b25c2d
Aayush Dharpure <158020844+Aayush-1104@users.noreply.github.com>
2025-08-12 13:09:31 +0530

    Time: 9 ms (57.45%), Space: 45.3 MB (47.61%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,12 +1,32 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int count[]=new int[26];
+        // One traversal
+        int Index[]=new int[26];
+        Arrays.fill(Index,-1);
         for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+            if(Index[s.charAt(i)-'a']==-1){
+                Index[s.charAt(i)-'a']=i;
+            }
+            else{
+                Index[s.charAt(i)-'a']=-2;
+            }
         }
-        for(int i=0;i<s.length();i++){
-            if(count[s.charAt(i)-'a']==1) return i;
+        int res=Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+            if(Index[i]>=0){
+                res=Math.min(res,Index[i]);
+            }
         }
-        return -1;
+        return (res==Integer.MAX_VALUE)?-1:res;
+
+        // // Two traversals
+        // int count[]=new int[26];
+        // for(int i=0;i<s.length();i++){
+        //     count[s.charAt(i)-'a']++;
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     if(count[s.charAt(i)-'a']==1) return i;
+        // }
+        // return -1;
     }
 }
